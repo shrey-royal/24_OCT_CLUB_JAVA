56b00124972bf077ab5e78d2816f2972e509a418
Shrey K <srkkadia@gmail.com>
2025-01-17 18:49:55 +0530

    Packages

---
+++ OOPs/Encapsulation/PackageUsage.java
@@ -0,0 +1,16 @@
+import mypack.MathUtils;
+
+public class PackageUsage {
+    public static void main(String[] args) {
+        System.out.println("Addition: " + MathUtils.add(10, 3));
+        System.out.println("Addition: " + MathUtils.subtract(10, 8));
+    }
+}
+
+/*
+Package is used to categorize the classes and interfaces so that they can be easily maintained.
+
+> Multiple classes can be defined in a single package.
+
+
+*/
---
+++ OOPs/Encapsulation/mypack/MathUtils.java
@@ -0,0 +1,11 @@
+package mypack;
+
+public class MathUtils {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
+}
